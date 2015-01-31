7884503c18fe8ead6308f6c83975c1a42e6c1274
Sam Billingham <sambillie.sb@gmail.com>
2015-01-31 19:55:03 +0000

    add writer v2 dmeo

---
+++ demos/writer-2/js/main.js
@@ -0,0 +1,53 @@
+var jPM = {};
+
+$(function() {
+
+    jPM = $.jPanelMenu({
+
+        menu : '#menu-target',
+        trigger: '.menu-trigger',
+        animated: false,
+        beforeOpen : ( function() {
+            // if (matchMedia('only screen and (min-width: 992px)').matches) {
+            //     $('.sidebar').css("left", "250px");
+            // }
+        }),
+        beforeClose : ( function() {
+            // $('.sidebar').css("left", "0");
+            $('.writer-icon, .side-writer-icon').removeClass("fadeOutUp");
+        })
+    });
+
+    jPM.on();
+
+    $('.select-posts, .select-categories').on('click', function () {
+
+        $('.home-page-posts').toggleClass("hide");
+        $('.home-page-categories').toggleClass("hide");
+
+        $('.select-posts').toggleClass("active");
+        $('.select-categories').toggleClass("active");
+
+        $('.home-footer').toggleClass("hide");
+    });
+
+    $('.writer-icon').on('click', function () {
+        $(this).addClass("fadeOutUp");
+
+    });
+
+    var fullHeight = $(window).height();
+
+    $('.hero-image-404').css("height", fullHeight );
+
+
+    console.log("T Test");
+
+    $('.sidebar').on('click', function(){
+        return true;
+    });
+
+    // Add Bg colour from Js so jPanel has time to initalize
+    $('body').css({"background-color":"#333337"});
+
+});
