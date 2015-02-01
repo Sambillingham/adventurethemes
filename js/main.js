7647e5645c7e80de853ccb83f8709d85367444dd
Sam Billingham <sambillie.sb@gmail.com>
2015-02-01 18:00:55 +0000

    DELETE ALL

--- js/main.js
+++
@@ -1,8 +0,0 @@
-
-$('.theme-preview').hover( function(){
-    $(this).children('a').fadeIn(200);
-    $(this).children('.overlay').fadeIn(200);
-}, function(){
-    $(this).children('a').fadeOut(200);
-    $(this).children('.overlay').fadeOut(200);
-});
