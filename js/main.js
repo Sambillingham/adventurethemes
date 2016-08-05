8b0ad9ad76cb1919d6f35aaeaa3761578f63a82d
Sam Billingham <sambillie.sb@gmail.com>
2016-08-05 23:03:39 +0100

    start responsive styles home

--- js/main.js
+++ js/main.js
@@ -2,28 +2,38 @@
 $(document).ready(function(){
 
     var instance = $('header').data("vide");
-    // Get video element of the background. Do what you want.
-    var videoObject = instance.getVideoObject();
-    //pause the video
-    videoObject.play();
+    console.log(instance);
 
-    console.log( videoObject );
+    // check if video is avalible
+    if (instance.getVideoObject() !== null ){
+        var videoObject = instance.getVideoObject();
+        //pause the video
+        console.log( videoObject );
+        videoObject.play();
 
-    videoObject.onplay = function () {
-        $('header').velocity({
-                        backgroundColorAlpha: 0.8,
-                        colorRed: "40",
-                        colorGreen: "40",
-                        colorBlue: "40",
-                    });
-        $('.vide-wrapper').velocity({opacity : 1}, 700);
 
-    };
+        videoObject.onplay = function () {
+          $('header').velocity({
+            backgroundColorAlpha: 0.8,
+            colorRed: "40",
+            colorGreen: "40",
+            colorBlue: "40",
+          });
+          $('.vide-wrapper').velocity({opacity : 1}, 700);
 
-    $('.js-logo-icon').delay(500).velocity({opacity : 1}, 1000, function(){
+        };
+        $('.js-logo-icon').delay(500).velocity({opacity : 1}, 1000, function(){
+            $(this).velocity({ "margin-left": 0}, 600);
+            $('.js-logo-type').velocity({opacity : 1}, 800);
+        });
+    } else {
+      $('.vide-wrapper').velocity({opacity : 1}, 700);
+      $('.js-logo-icon').delay(500).velocity({opacity : 1}, 1000, function(){
         $(this).velocity({ "margin-left": 0}, 600);
         $('.js-logo-type').velocity({opacity : 1}, 800);
-    });
+      });
+    }
+
 
     function getQueryVariable(variable){
        var query = window.location.search.substring(1);
@@ -40,5 +50,3 @@ $(document).ready(function(){
     }
 
 });
-
-
